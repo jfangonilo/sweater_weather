4eb173c9974c25848fbf918501fc59f82a9284e3
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-03-01 11:32:18 -0700

    Add background poro

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -2,6 +2,9 @@ class Api::V1::BackgroundsController < ApplicationController
   def index
     location = params[:location]
     service = FlickrService.new(location)
-    service.backgrounds
+    background_collection = service.backgrounds[:photos][:photo]. map do |data|
+      Background.new(data)
+    end
+    background = background_collection.sample.img_url
   end
 end
