0b11765fe8b2522bf7926c4f5024b85077b68347
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-03-01 14:20:06 -0700

    Add faraday conn with geocoding

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,5 +1,12 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-
+    location = params[:location]
+    conn = Faraday.new('https://maps.googleapis.com/maps/api/geocode/json') do |f|
+      f.adapter Faraday.default_adapter
+      f.params[:key] = ENV['GOOGLE_GEOCODING_KEY']
+      f.params[:address] = "#{location}"
+    end
+    response = conn.get
+    parsed = JSON.parse(response.body, symbolize_names: true)
   end
 end
