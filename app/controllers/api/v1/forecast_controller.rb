9544bddbce2f45210c527aa575617ba76a75dbf8
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-03-01 15:06:09 -0700

    Add weather forecast to controller action

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,6 +1,17 @@
 class Api::V1::ForecastController < ApplicationController
   def index
     forecast = ForecastFacade.new(params[:location])
-    render json: ForecastSerializer.new(forecast)
+    location = forecast.lat_lng
+    binding.pry
+    lat = location[:lat]
+    lng = location[:lng]
+
+    conn = Faraday.new("https://api.darksky.net/forecast/#{ENV['DARKSKY_KEY']}/#{lat},#{lng}") do |f|
+      f.adapter Faraday.default_adapter
+    end
+    response = conn.get
+    weather = JSON.parse(response.body, symbolize_names: true)
+
+    # render json: ForecastSerializer.new(forecast)
   end
 end
