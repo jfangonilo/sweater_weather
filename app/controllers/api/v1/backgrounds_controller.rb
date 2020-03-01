6e13d1207ddccd118b9bb68c00f5bbf3d5c9a625
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-03-01 10:47:03 -0700

    Add initial flickr service connection

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,5 +1,16 @@
 class Api::V1::BackgroundsController < ApplicationController
   def index
-
+    conn = Faraday.new('https://www.flickr.com/services/rest') do |f|
+      f.adapter Faraday.default_adapter
+      f.params[:method] = 'flickr.photos.search'
+      f.params[:api_key] = ENV['FLICKR_KEY']
+      f.params[:geo_context] = 2 #outdoors
+      f.params[:text] = "#{params[:location]}"
+      f.params[:tags] = "#{params[:location]},skyline,weather"
+      f.params[:format] = 'json'
+      f.params[:nojsoncallback] = 1
+    end
+    response = conn.get
+    json = JSON.parse(response.body)
   end
 end
