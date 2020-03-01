56d67c9a5a5f48fdd6858ff91b9fec9cbe840627
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-03-01 11:51:45 -0700

    Add serializer

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -2,5 +2,6 @@ class Api::V1::BackgroundsController < ApplicationController
   def index
     location = params[:location]
     background = BackgroundFacade.new(location)
+    render json: BackgroundSerializer.new(background)
   end
 end
