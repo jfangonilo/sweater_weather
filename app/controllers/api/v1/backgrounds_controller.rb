bb9d6fdbdbac84804a0f3b046dc2ff9eed5d546e
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-03-01 11:45:45 -0700

    Add background facade

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,10 +1,6 @@
 class Api::V1::BackgroundsController < ApplicationController
   def index
     location = params[:location]
-    service = FlickrService.new(location)
-    background_collection = service.backgrounds[:photos][:photo]. map do |data|
-      Background.new(data)
-    end
-    background = background_collection.sample.img_url
+    background = BackgroundFacade.new(location)
   end
 end
---
+++ app/facades/background_facade.rb
@@ -0,0 +1,22 @@
+class BackgroundFacade
+  attr_reader :id
+
+  def initialize(location)
+    @location = location
+    @id = nil
+  end
+
+  def url
+    background_collection.sample.img_url
+  end
+
+  def background_collection
+    service.backgrounds[:photos][:photo]. map do |data|
+      Background.new(data)
+    end
+  end
+
+  def service
+    @service ||= FlickrService.new(@location)
+  end
+end
