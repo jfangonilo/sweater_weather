87981faa46383b3535c1b669d54582cf7e6fa9db
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-03-01 18:32:17 -0700

    Update time attribute

--- app/models/daily_weather.rb
+++ app/models/daily_weather.rb
@@ -1,12 +1,12 @@
 class DailyWeather
-  attr_reader :day,
+  attr_reader :time,
               :summary,
               :chance_precip,
               :high_temp,
               :low_temp
 
   def initialize(daily_data)
-    @day = daily_data[:time]
+    @time = daily_data[:time]
     @summary = daily_data[:summary]
     @chance_precip = daily_data[:precipProbability]
     @high_temp = daily_data[:temperatureHigh]
