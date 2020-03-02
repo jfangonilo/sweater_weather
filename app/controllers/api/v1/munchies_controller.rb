f9bd78d83d51997e2c5c3193a00e7c54b9a37b22
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-03-02 10:27:43 -0700

    Rename munchie

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::MunchiesController < ApplicationController
   def index
     munchies = MunchiesFacade.new(params)
-    render json: MunchiesSerializer.new(munchies)
+    render json: MunchieSerializer.new(munchies)
   end
 end
--- app/serializers/munchie_serializer.rb
+++ app/serializers/munchie_serializer.rb
@@ -1,4 +1,4 @@
-class MunchiesSerializer
+class MunchieSerializer
   include FastJsonapi::ObjectSerializer
   attributes  :id,
               :end_location,
