f5ca151a8d7c4947ff0cd6fa9dc86d62bda16208
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-03-01 17:37:30 -0700

    Update serializer

--- app/serializers/forecast_serializer.rb
+++ app/serializers/forecast_serializer.rb
@@ -1,4 +1,8 @@
 class ForecastSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :city_state
+  attributes  :id,
+              :current_weather,
+              :details,
+              :hourly_weather,
+              :daily_weather
 end
