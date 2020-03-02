e1227d52c80608aa6db22fe473f018a0ecfa24f3
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-03-01 17:49:25 -0700

    Update time attr

--- app/models/current_weather.rb
+++ app/models/current_weather.rb
@@ -12,6 +12,6 @@ class CurrentWeather
     @temperature = forecast[:currently][:temperature]
     @high_temp = forecast[:daily][:data][0][:temperatureHigh]
     @low_temp = forecast[:daily][:data][0][:temperatureLow]
-    @time = DateTime.now.strftime("%l:%M %p, %_m/%-d")
+    @time = DateTime.now
   end
 end
