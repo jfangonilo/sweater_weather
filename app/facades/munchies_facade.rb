f6176daefae43aa4dd7a383756741cc1905074d5
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-03-02 09:46:28 -0700

    Add travel time

--- app/facades/munchies_facade.rb
+++ app/facades/munchies_facade.rb
@@ -1,5 +1,21 @@
 class MunchiesFacade
-  def initialize(params)
+  attr_reader :start,
+              :end,
+              :food,
+              :params
 
+  def initialize(params)
+    @start = params[:start]
+    @end = params[:end]
+    @food = params[:food]
+    @params = params
+  end
+
+  def travel_time
+    directions.travel_time
+  end
+
+  def directions
+    @geocode ||= DirectionsService.new(params)
   end
 end
---
+++ app/services/directions_service.rb
@@ -0,0 +1,27 @@
+class DirectionsService
+  attr_reader :origin,
+              :destination
+
+  def initialize(params)
+    @origin = params[:start]
+    @destination = params[:end]
+  end
+
+  def travel_time
+    get_json[:routes][0][:legs][0][:duration][:value]
+  end
+
+  def get_json
+    response = conn.get
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new('https://maps.googleapis.com/maps/api/directions/json') do |f|
+      f.adapter Faraday.default_adapter
+      f.params[:key] = ENV['GOOGLE_DIRECTIONS_KEY']
+      f.params[:origin] = "#{origin}"
+      f.params[:destination] = "#{destination}"
+    end
+  end
+end
