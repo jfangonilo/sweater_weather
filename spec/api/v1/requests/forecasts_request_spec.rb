032048bbcb405bc730e89e8cc47b4479ec2be2e0
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-03-01 18:37:02 -0700

    Update forecast request spec

--- spec/api/v1/requests/forecasts_request_spec.rb
+++ spec/api/v1/requests/forecasts_request_spec.rb
@@ -9,5 +9,8 @@ RSpec.describe "Sweather Weather API" do
     expect(response).to be_successful
     forecast = JSON.parse(response.body, symbolize_names: true)
     expect(forecast[:data][:type]).to eq 'forecast'
+    expect(forecast[:data][:attributes][:current_weather][:location]). to eq "Denver, CO, USA"
+    expect(forecast[:data][:attributes][:hourly_weather].length).to eq 8
+    expect(forecast[:data][:attributes][:daily_weather].length).to eq 5
   end
 end
