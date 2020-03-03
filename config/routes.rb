b7e9f0a67a5335bc4115cb16ec415937ecc02225
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-03-02 19:12:46 -0700

    Add user create route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :backgrounds, only: [:index]
       resources :forecast, only: [:index]
+      resources :users, only: [:create]
     end
   end
 end
