292b4e186d22b68a0e695304ac8e6b60d4495eaa
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-03-02 20:58:37 -0700

    Add directions service

---
+++ app/services/directions_service.rb
@@ -0,0 +1,28 @@
+class DirectionsService
+  attr_reader :origin, :destination
+
+  def initialize(origin, destination)
+    @origin = origin
+    @destination = destination
+  end
+
+  def travel_time
+    get_json[:routes][0][:legs][0][:duration][:text]
+  end
+
+  private
+
+  def get_json
+    response = conn.get
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new('https://maps.googleapis.com/maps/api/directions/json') do |f|
+      f.adapter Faraday.default_adapter
+      f.params[:key] = ENV['GOOGLE_DIRECTIONS_KEY']
+      f.params[:origin] = "#{origin}"
+      f.params[:destination] = "#{destination}"
+    end
+  end
+end
