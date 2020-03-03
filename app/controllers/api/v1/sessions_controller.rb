6d253e2bae5d121c2c2c85c9ad7ef490e0e2a98f
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-03-02 19:56:38 -0700

    Add session create action

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::SessionsController < ApplicationController
   def create
-    binding.pry
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      render status: 200, json: UsersSerializer.new(user)
+    else
+      render status: 401, json: {data: "Bad Credentials"}
+    end
   end
 end
