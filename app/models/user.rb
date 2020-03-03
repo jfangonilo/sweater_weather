41f811b7944181ddf960376f71f9977fd14ce0b1
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-03-02 19:33:14 -0700

    Add email uniqueness validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   validates_presence_of :email,
                         :api_key
 
+  validates_uniqueness_of :email
   validates_confirmation_of :password
   has_secure_password
 end
