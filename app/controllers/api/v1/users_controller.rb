ec0d02e516ce445594e92a741dffb2655402acf9
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-03-03 08:14:11 -0700

    Switch to has_unique_token

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(user_params)
-    user.update(api_key: SecureRandom.urlsafe_base64.to_s)
+    # user.update(api_key: SecureRandom.urlsafe_base64.to_s)
     if user.save
       render status: 201, json: UsersSerializer.new(user)
     else
