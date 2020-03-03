748ce78240600b4753cecac1715995505e821ab8
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-03-02 19:51:57 -0700

    Add sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       resources :backgrounds, only: [:index]
       resources :forecast, only: [:index]
       resources :users, only: [:create]
+      resources :sessions, only: [:create]
     end
   end
 end
