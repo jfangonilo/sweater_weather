684fbe70bc44834fc55e716fdfa1646f93d9f7be
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-03-02 20:39:31 -0700

    Add uniqueness validation for api key

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :email}
     it { should validate_uniqueness_of :email}
     it { should validate_presence_of :api_key}
+    it { should validate_uniqueness_of :api_key}
     it { should validate_confirmation_of :password}
   end
 end
